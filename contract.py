271b91d37c11ea9b6fc68f1b2d186d178fd94ffa
paul-laird <35932444+paul-laird@users.noreply.github.com>
2021-12-07 18:50:02 +0000

    Update contract.py
    typo

--- contract.py
+++ contract.py
@@ -7,7 +7,7 @@ from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
 
-with open("Greeter.sol.sol") as c:
+with open("Greeter.sol") as c:
   contractText=c.read()
 with open(".pk") as pkfile:
   privateKey=pkfile.read()
