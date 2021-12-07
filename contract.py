1e622f04a8792c2d21bb1cb10c1b64ab3ae5bc60
paul-laird <35932444+paul-laird@users.noreply.github.com>
2021-12-07 18:49:25 +0000

    Update contract.py
    imports and file reads

--- contract.py
+++ contract.py
@@ -1,1 +1,17 @@
-print('hello world')
+import time
+import json
+import web3
+from eth_account import Account
+from web3.auto import w3
+from web3.providers.websocket import WebsocketProvider
+from web3 import Web3
+from solc import compile_standard
+
+with open("Greeter.sol.sol") as c:
+  contractText=c.read()
+with open(".pk") as pkfile:
+  privateKey=pkfile.read()
+with open(".infura") as infurafile:
+  infuraKey=infurafile.read()
+
+print(contractText)
