be42ba678c91d26137d422a2465840f2b4fa3f17
paul-laird <35932444+paul-laird@users.noreply.github.com>
2021-12-07 10:09:01 +0000

    Update old_contract.py
    rename .sol file

--- old_contract.py
+++ old_contract.py
@@ -6,7 +6,7 @@ from web3.auto import w3
 from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
-with open("contract.sol") as c:
+with open("Greeter.sol") as c:
   contractText=c.read()
 with open(".pk") as pkfile:
   privateKey=pkfile.read()
